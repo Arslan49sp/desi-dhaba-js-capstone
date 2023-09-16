ab4335ff8406118f3b6e0b0a1ab7b2a753189e80
Arslan Majeed <arslanmajeed214@gmail.com>
2023-09-17 03:45:31 +0500

    fetch all the items from api and then show them to home page.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './style.css'
+import "./style.css";
+import loadItemCard from "./modules/loadItemsCard.js";
+import loadAllCard from "./modules/loadItemData.js";
+
+const allItem = await loadAllCard();
+loadItemCard(allItem);
