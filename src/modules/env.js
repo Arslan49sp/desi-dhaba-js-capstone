c04a12810bdbd7e079b40c659c53f46ade1375b2
Arslan Majeed <arslanmajeed214@gmail.com>
2023-09-17 07:56:31 +0500

    Load all comments of relevant item.

--- src/modules/env.js
+++ src/modules/env.js
@@ -1,4 +1,4 @@
-const APP_ID = 'G55EMoceVwwwFqu29gdn';
+export const APP_ID = 'G55EMoceVwwwFqu29gdn';
 
 const INV_URL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/likes/`;
 export default INV_URL;
