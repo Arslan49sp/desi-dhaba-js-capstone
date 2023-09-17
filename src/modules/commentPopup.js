87105b2cf3e5178814b3794f61bd2306aa9585e2
Arslan Majeed <arslanmajeed214@gmail.com>
2023-09-17 08:17:41 +0500

    Fetch the information from form and store in to the server

--- src/modules/commentPopup.js
+++ src/modules/commentPopup.js
@@ -1,13 +1,14 @@
-import com from './showComments.js';
+import com from "./showComments.js";
+import postComments from "./postComments.js";
 
 const display = (items) => {
-  const COMMENTS = document.querySelectorAll('.comment');
+  const COMMENTS = document.querySelectorAll(".comment");
   COMMENTS.forEach((comment) => {
-    comment.addEventListener('click', () => {
-      const id = comment.getAttribute('data-id');
+    comment.addEventListener("click", () => {
+      const id = comment.getAttribute("data-id");
       const item = items.find((o) => o.idMeal === id);
-      const parmodal = document.querySelector('.parmodal');
-      parmodal.style.display = 'grid';
+      const parmodal = document.querySelector(".parmodal");
+      parmodal.style.display = "grid";
       parmodal.innerHTML = `
               <div class="parmodal__modal">
                 <div class="front"><button id="close" class="close">&times;</button></div>
@@ -47,11 +48,27 @@ const display = (items) => {
                 </div>
               </div>
             `;
-      const close = document.querySelector('#close');
-      close.addEventListener('click', () => {
-        parmodal.style.display = 'none';
+      const close = document.querySelector("#close");
+      close.addEventListener("click", () => {
+        parmodal.style.display = "none";
       });
       com(id);
+
+      const Name = document.querySelector("#name");
+      const Textarea = document.querySelector("#textarea");
+      const submit = document.querySelector("#submit");
+      submit.addEventListener("click", (e) => {
+        e.preventDefault();
+        postComments({
+          item_id: id,
+          username: Name.value,
+          comment: Textarea.value,
+        }).then((res) => {
+          if (res.status === 201) {
+            com(id);
+          }
+        });
+      });
     });
   });
 };
