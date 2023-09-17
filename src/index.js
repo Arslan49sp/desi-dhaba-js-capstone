f8b27e85e52e402795a7aca40519f28fea43e125
Arslan Majeed <arslanmajeed214@gmail.com>
2023-09-17 06:22:30 +0500

    show counting on the page.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import './style.css';
-import loadItemCard from './modules/loadItemsCard.js';
-import loadAllCard from './modules/loadItemData.js';
-import { loadLikes } from './modules/loadLikes.js';
+import "./style.css";
+import loadItemCard from "./modules/loadItemsCard.js";
+import loadAllCard from "./modules/loadItemData.js";
+import totalItemsCounter from "./modules/totalItemsCounter.js";
+import { loadLikes } from "./modules/loadLikes.js";
 
 const allItem = await loadAllCard();
 loadItemCard(allItem);
 await loadLikes();
+totalItemsCounter(allItem.length);
